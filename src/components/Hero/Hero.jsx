e15315c4f394b0512914899e59380cec7080fb09
Amay <brajbliss@gmail.com>
2022-07-28 19:29:00 +0530

    done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,30 @@
+import axios from 'axios';
+import { useState } from 'react';
 import './hero.scss';
 
-import React from 'react';
-
 const Hero = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		await axios
+			.post('https://reqres.in/api/login', { email, password })
+			.then((response) => {
+				alert(
+					`Login successful with following Token: ${response.data.token}`
+				);
+			})
+			.catch((error) => {
+				alert(
+					`Following Error Occurred: ${Object.values(
+						error.response.data
+					)}`
+				);
+			});
+	};
+
 	return (
 		<div className='hero-container'>
 			<div className='hero-wrapper'>
@@ -17,27 +39,32 @@ const Hero = () => {
 							className='form-input'
 							placeholder='Email Address*'
 							required
+							onChange={(e) => setEmail(e.target.value)}
 						/>
 						<input
 							type='password'
 							className='form-input'
 							placeholder='Password*'
 							required
+							onChange={(e) => setPassword(e.target.value)}
 						/>
-						<button className='form-submit-btn'>Login</button>
+						<button
+							className='form-submit-btn'
+							type='submit'
+							onClick={handleSubmit}>
+							Login
+						</button>
 					</div>
 					<div className='more'>
 						<div className='more-links'>
-							<div className='more-link1 links'>
+							<div className='more-link1'>
 								<input
 									type='checkbox'
 									className='more-link-checkbox'
 								/>
 								Remember Password
 							</div>
-							<div className='more-link2 links'>
-								Forgot Password?
-							</div>
+							<div className='more-link2'>Forgot Password?</div>
 						</div>
 					</div>
 				</div>
